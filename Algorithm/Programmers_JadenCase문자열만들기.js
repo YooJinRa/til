2903c41ab8ad9ab245dc141e6f7037ce96fea2e8
YooJinRa <rarastudy@naver.com>
2022-10-31 23:05:51 +0900

    ✨new : Algorithm Programmers JadenCase

--- Algorithm/Programmers_JadenCase문자열만들기.js
+++ Algorithm/Programmers_JadenCase문자열만들기.js
@@ -1,3 +1,8 @@
+// 문제 설명
+// JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열입니다.
+// 단, 첫 문자가 알파벳이 아닐 때에는 이어지는 알파벳은 소문자로 쓰면 됩니다. (첫 번째 입출력 예 참고)
+// 문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
+
 function solution(s) {
   const word = String(s).split(' ');
   let changeSentense = [];
